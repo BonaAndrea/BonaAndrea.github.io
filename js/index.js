2fd53a7e0e053451bf4b05a42ae884f30a4d0b0c
Andrea <andrea.bona1996@gmail.com>
2023-04-12 23:26:07 +0200

    Full implementation

--- js/index.js
+++ js/index.js
@@ -53,15 +53,14 @@ window.onscroll = () => {
     }
 };
 
-
-
-// scroll reveal
-ScrollReveal({
-    //reset: true,
-    distance: '80px',
-    duration: 2000, 
-    delay: 200
-});
+if (sectionId === "" || sectionId === "home") {
+    ScrollReveal({
+        //reset: true,
+        distance: '80px',
+        duration: 2000, 
+        delay: 200
+    });
+}
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
