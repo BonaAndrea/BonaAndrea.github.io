b943082c2bd97d0ff2d1438f974ba8f25cc82e43
Andrea Bona <andrea.bona1996@gmail.com>
2023-11-02 18:22:16 +0100

    Fixed reveal demos-box

--- js/index.js
+++ js/index.js
@@ -63,7 +63,7 @@ if (sectionId === "" || sectionId === "home") {
 }
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .demos-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
